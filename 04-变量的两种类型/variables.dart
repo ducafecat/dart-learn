5e0086e281062d5582e6d15368c954dd515fd8e6
ducafecat <ducafecat@gmail.com>
2018-10-10 10:59:58 +0800

    添加 => 04-变量的两种类型

---
+++ 04-变量的两种类型/variables.dart
@@ -0,0 +1,42 @@
+void main() {
+  // 弱类型
+  // var a;
+  // a = 'ducafecat';
+  // a = 123;
+  // a = true;
+  // a = {'key': 'val123'};
+  // a = ['abc'];
+  // print(a);
+
+  // 强类型
+  // String a;
+  // a = 'ducafecat';
+  // a = 123;
+  // print(a);
+
+  // 动态定义 dynamic
+  // dynamic a = 'doucafecat';
+  // a = 123;
+  // a = [1111];
+  // print(a);
+
+  // 动态定义 Object
+  // Object a = 'doucafecat';
+  // a = 123;
+  // a = [2222];
+  // print(a);
+
+  // 默认值
+  // var a;
+  // String a;
+  // print(a);
+
+  // 常用强类型
+  // String a = 'doucafecat';
+  // int i = 123;
+  // double d = 0.12;
+  // bool b = true;
+  // DateTime dt = new DateTime.now();
+  // List l = [ a, i, d, b, dt];
+  // print(l);
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Dart语言学习 【配套代码】
+
+## 文档
+
+[ducafecat.tech](https://ducafecat.tech/categories/Dart%E8%AF%AD%E8%A8%80%E5%AD%A6%E4%B9%A0/)
+
+## 目录
+
+- 04-变量的两种类型
