cb85a400e6eb6586cb9a7cb08df47fae46bd2f6b
ducafecat <ducafecat@gmail.com>
2018-10-18 14:50:23 +0800

    添加 => 08 字符串

---
+++ 08-字符串/string.dart
@@ -0,0 +1,70 @@
+void main() {
+  // 单引号或者双引号
+  // String a = 'ducafecat';
+  // String b = "ducafecat";
+
+
+  // 字符串模板 ${expression} 表达式
+  // var a = 123;
+  // String b = 'ducafecat : ${a}';
+  // print(b);
+
+
+  // 字符串连接
+  // var a = 'hello' + ' ' + 'ducafecat';
+  // var a = 'hello'' ''ducafecat';
+  // var a = 'hello'   ' '     'ducafecat';
+  // var a = 'hello'
+  // ' '
+  // 'ducafecat';
+  // var a = '''
+  // hello word
+  // this is multi line
+  // ''';
+  // var a = """
+  // hello word
+  // this is multi line
+  // """;
+  // print(a);
+
+  // 转义符号
+  // var a = 'hello word \n this is multi line';
+  // print(a);
+
+  // 取消转义 raw 字符串
+  // var a = r'hello word \n this is multi line';
+  // print(a);
+
+  // 搜索
+  // var a = 'web site ducafecat.tech';
+  // print(a.contains('ducafecat'));
+  // print(a.startsWith('web'));
+  // print(a.endsWith('tech'));
+  // print(a.indexOf('site'));
+
+  // 提取数据
+  // print(a.substring(0,5));
+  // var b = a.split(' ');
+  // print(b.length);
+  // print(b[0]);
+
+  // 大小写转换
+  // print(a.toLowerCase());
+  // print(a.toUpperCase());
+
+  // 裁剪 判断空字符串
+  // print('    hello word     '.trim());
+  // print(''.isEmpty);
+
+  // 替换部分字符
+  // print('hello word word!'.replaceAll('word', 'ducafecat'));
+
+  // 字符串创建
+  // var sb = StringBuffer();
+  // sb..write('hello word!')
+  // ..write('my')
+  // ..write(' ')
+  // ..writeAll(['web', 'site', 'https://ducafecat.tech']);
+  // print(sb.toString());
+
+}
