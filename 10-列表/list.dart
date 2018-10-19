16dfd0fb3d41ea26d8eca2d70a82c4aa16380d67
ducafecat <ducafecat@gmail.com>
2018-10-19 11:02:53 +0800

    添加 => 10 列表

---
+++ 10-列表/list.dart
@@ -0,0 +1,95 @@
+void main() {
+  // 初始
+  // var l = [1, 2, 3];
+  // print(l);
+
+
+  // 声明
+
+  // 定长
+  // List<int> l = new List();
+  // l
+  // ..add(1)
+  // ..add(2)
+  // ..add(3);
+  // print(l);
+
+  // 自动
+  // List<int> l = new List(3);
+  // // print(l[0]);
+  // l[0] = 1;
+  // l[1] = 2;
+  // l[2] = 3;
+  // print(l);
+
+
+  // 属性
+  // var l = [1, 2, 3];
+  // print(l.first);
+  // print(l.last);
+  // print(l.length);
+  // print(l.isEmpty);
+  // print(l.isNotEmpty);
+
+
+  // 方法
+  // List<int> l = new List();
+  
+  // 添加
+  // l
+  //   ..add(1)
+  //   ..addAll([2, 3, 4, 5])
+  //   ..insert(0, 6)
+  //   ..insertAll(6, [6, 6])
+  //   ;
+
+  // 查询
+  // print(l.indexOf(5));
+  // print(l.indexWhere((it) => it == 4));
+
+  // 删除
+  // l.remove(6);
+  // print(l);
+  // l.removeAt(5);
+  // print(l);
+
+  // range 操作
+  // l.fillRange(0, 3, 9);
+  // print(l.getRange(0, 5));
+
+  // 洗牌
+  // l.shuffle();
+  // print(l);
+  // l.shuffle();
+  // print(l);
+
+  // 排序
+  // 数字
+  // l.sort();
+  // print(l);
+  // 日期
+  // List<DateTime> dtList = new List();
+  // dtList.addAll([
+  //   DateTime.now(),
+  //   DateTime.now().add(new Duration(days: -12)),
+  //   DateTime.now().add(new Duration(days: -2))
+  //   ]);
+  // print(dtList);
+  // dtList.sort((a, b) => a.compareTo(b));
+  // print(dtList);
+  // 反正
+  // print(dtList.reversed);
+
+  // 复制子列表
+  // print(l);
+  // var l2 = l.sublist(1,4);
+  // print(l2);
+
+  // 操作符
+  // var l1 = [1, 2, 3];
+  // var l2 = [4, 5, 6];
+  // print(l1 + l2);
+  // l1[2] = 9;
+  // print(l1[2]);
+
+}
