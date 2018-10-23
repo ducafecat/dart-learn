8658e6a683b24f89b99fa6355caafd18edb02c37
ducafecat <ducafecat@gmail.com>
2018-10-23 17:06:53 +0800

    添加 => 11 Map

---
+++ 11-Map/map.dart
@@ -0,0 +1,50 @@
+void main() {
+  // 初始
+  // var a = {'name': 'ducafecat', 'web': 'www.ducafecat.tech'};
+  // print(a);
+
+  // 声明
+  // 松散
+  // var a = new Map();
+  // a['name'] = 'ducafecat';
+  // a['web'] = 'www.ducafecat.tech';
+  // a[0] = 'abc';
+  // print(a);
+
+  // 定义 k v 类型
+  // var b = new Map<int, String>();
+  // b[0] = 'java';
+  // b[1] = 'python';
+  // print(b);
+
+  // print(a);
+  
+  // 基本属性
+  // print(a.isEmpty);
+  // print(a.isNotEmpty);
+  // print(a.keys);
+  // print(a.values);
+  // print(a.length);
+  // print(a.entries);
+
+  // 常用方法
+  // b.addAll(a);
+  // b.addAll({'first': 'java', 'second': 'python'});
+  // b.addEntries(a.entries);
+  // print(b);
+  // b.clear();
+  // print(b);
+  // print(a.containsKey('name'));
+  // print(a.containsValue('www.ducafecat.tech'));
+  // a.remove('name');
+  // a.removeWhere((key,val) => key == 'name');
+  // a.update('name', (val) => 'abc');
+  // a.updateAll((key, val) => "---$val---");
+  // print(a);
+
+  // 操作符
+  // print(a['name']);
+  // a['name'] = 'abc';
+  // print(a);
+
+}
