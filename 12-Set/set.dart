01c5df7a3123e9ed0d3d8f64b9d11699e0442693
ducafecat <ducafecat@gmail.com>
2018-10-25 15:21:34 +0800

    添加 => 12 Set

---
+++ 12-Set/set.dart
@@ -0,0 +1,48 @@
+void main() {
+  // 声明
+  // var a = new Set();
+  // a.add('java');
+  // a.add('php');
+  // a.add('python');
+  // a.add('java');
+  // a.add('sql');
+  // a.add('swift');
+  // a.add('dart');
+  // print('a => $a');
+
+  // 强类型
+  // var b = new Set<String>();
+  // b.addAll(['dart', 'c#', 'j#', 'e#']);
+
+  // 属性
+  // print(a.isEmpty);
+  // print(a.isNotEmpty);
+  // print(a.first);
+  // print(a.last);
+  // print(a.length);
+
+
+  // 常用方法
+  // b.addAll(['dart', 'c#', 'j#', 'e#']);
+  // print(b.contains('dart'));
+  // print(b.containsAll(['dart', 'swift']));
+  // print('b => $b');
+  // print(a.difference(b));
+  // print(a.intersection(b));
+  // print(b.lookup('dart'));
+  // print(b.union(a));
+  // b.remove('dart');
+  // b.removeAll(['dart','c#']);
+  // b.clear();
+  // print(b.firstWhere((it) => it == 'c#'));
+  // print(b.lastWhere((it) => it == 'c#'));
+  // b.removeWhere((it) => it == 'c#');
+  // b.retainAll(['e#']);
+  // b.retainWhere((it) => it == 'e#');
+  // b.retainWhere((it) {
+  //   bool ret = it == 'e#';
+  //   return ret;
+  // });
+  // print('b => $b');
+
+}
