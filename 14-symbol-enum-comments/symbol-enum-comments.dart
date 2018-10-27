b789cd6c4d5cd2b30a902d4323c6b283c03ed99f
ducafecat <ducafecat@gmail.com>
2018-10-27 12:00:03 +0800

    添加 => 14 symbol、enum、comments

---
+++ 14-symbol-enum-comments/symbol-enum-comments.dart
@@ -0,0 +1,23 @@
+// import 'dart:mirrors';
+
+enum Status { none, running, stopped, paused }
+
+/// `main` 函数
+///
+/// 符号
+/// 枚举
+///
+void main() {
+  /*
+   * Symbol
+   * 
+  Symbol libraryName = new Symbol('dart.core');
+  MirrorSystem mirrorSystem = currentMirrorSystem();
+  LibraryMirror libMirror = mirrorSystem.findLibrary(libraryName);
+  libMirror.declarations.forEach((s, d) => print('$s - $d')); 
+  */
+
+  // Enum
+  // print(Status.values);
+  // Status.values.forEach((it) => print('$it - index: ${it.index}'));
+}
