cc307e0e1834b8b2feca44865b2c74ff45fbe8cb
ducafecat <ducafecat@gmail.com>
2018-11-12 15:29:34 +0800

    添加 => 16 operator

---
+++ 16-操作符/operator.dart
@@ -0,0 +1,43 @@
+void main(){
+  // 优先级顺序
+  // int x = 1;
+  // int y = 2;
+  // if(x == 1 && y == 2){
+  //   print(x+y);
+  // }
+
+  // 算术操作符
+  // print(5/2);
+  // print(5~/2);
+  // print(5 % 2);
+
+  // 类型判定操作符
+  // int a = 123;
+  // String b = 'ducafecat';
+  // String c = 'abc';
+  // print(a as Object);
+  // print(b is String);
+  // print(c is! String);
+
+  // 条件表达式
+  // bool isFinish = true;
+  // String txtVal = isFinish ? 'yes' : 'no';
+  // print(txtVal);
+  // bool isFinish;
+  // isFinish = isFinish ?? false;
+  // print(isFinish);
+
+  // 级联操作符
+  // StringBuffer sb = new StringBuffer();
+  // sb
+  // ..write('hello')
+  // ..write('word')
+  // ..write('\n')
+  // ..writeln('doucafecat');
+  // print(sb);
+
+  // ?. 操作符
+  // String a;
+  // print(a?.length);
+
+}
