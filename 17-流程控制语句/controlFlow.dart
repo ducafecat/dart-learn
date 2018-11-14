c8cce8996af5e1e548e0792a9ee407fd1bf85bdd
ducafecat <ducafecat@gmail.com>
2018-11-14 11:39:43 +0800

    添加 => 17 流程控制语句

---
+++ 17-流程控制语句/controlFlow.dart
@@ -0,0 +1,73 @@
+void main() {
+  // if else
+  // bool isPrint = true;
+  // if (isPrint) {
+  //   print('hello');
+  // }
+
+  // for
+  // for (var i = 0; i < 5; i++) {
+  //   print(i);
+  // }
+
+  // while
+  // bool isDone = false;
+  // while(!isDone) {
+  //   print('is not done');
+  //   isDone = true;
+  // }
+
+  // do while
+  // bool isRunning = true;
+  // do {
+  //   print('is running');
+  //   isRunning = false;
+  // } while (isRunning);
+
+  // switch case
+  // String name = 'cat';
+  // switch (name) {
+  //   case 'cat':
+  //     print('cat');
+  //     break;
+  //   default:
+  //     print('not find');
+  // }
+
+  // break
+  // num i = 1;
+  // while(true) {
+  //   print('${i} - run');
+  //   i++;
+  //   if(i == 5) {
+  //     break;
+  //   }
+  // }
+
+  // continue
+  // for (var i = 0; i < 5; i++) {
+  //   if (i < 3) {
+  //     continue;
+  //   }
+  //   print(i);
+  // }
+
+  // continue 指定位置
+  // String command = "close";
+  // switch(command) {
+  //   case "open":
+  //     print("open");
+  //     break;
+  //   case "close":
+  //     print("close");
+  //     continue doClose;
+
+  //   doClose:
+  //   case "doClose":
+  //     print("DO_CLOSE");
+  //     break;
+
+  //   default:
+  //     print("-----");
+  // }
+}
