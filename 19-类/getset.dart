74fcaa1b4a0db8aa8b200d96427f949eeafe0bec
ducafecat <ducafecat@gmail.com>
2018-11-17 15:49:32 +0800

    添加 => get set

---
+++ 19-类/getset.dart
@@ -0,0 +1,27 @@
+// 定义 get set
+// class People {
+//   String _name;
+
+//   set pName(String value) {
+//     _name = value;
+//   }
+
+//   String get pName {
+//     return 'people is ${_name}';
+//   }
+// }
+
+// 简化 get set
+// class People {
+//   String _name;
+
+//   set pName(String value) => _name = value;
+
+//   String get pName => 'people is ${_name}';
+// }
+
+void main() {
+  // var p = new People();
+  // p.pName = 'ducafecat';
+  // print(p.pName);
+}
