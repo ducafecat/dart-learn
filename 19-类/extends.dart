aa1ab751033ad7242ce46ed84a32aecb34303eb7
ducafecat <ducafecat@gmail.com>
2018-11-29 09:41:41 +0800

    添加 => extends

---
+++ 19-类/extends.dart
@@ -0,0 +1,36 @@
+// void main() {
+//   var p = AndroidPhone(12345678);
+//   p.showNumber();
+//   // p.startup();
+//   // p.shutdown();
+// }
+
+// class Phone {
+//   void startup() {
+//     print('开机');
+//   }
+//   void shutdown() {
+//     print('关机');
+//   }
+// }
+
+// class Mobile {
+//   int number;
+//   int signal;
+//   Mobile(this.number);
+//   void showNumber() {
+//     print('010-${number}');
+//   }
+// }
+
+// class AndroidPhone extends Mobile {
+//   AndroidPhone(int number) : super(number);
+//   // // int signal;
+//   // void startup() {
+//   //   super.startup();
+//   //   print('AndroidPhone 开机');
+//   // }
+//   // // void shutdown() {
+//   // //   print('AndroidPhone 关机');
+//   // // }
+// }
