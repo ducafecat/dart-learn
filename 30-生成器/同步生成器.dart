fb47571d4b8765aa4107098647396b87355a2dcf
ducafecat <ducafecat@gmail.com>
2019-01-15 17:34:13 +0800

    更新 =>代码顺序

--- 30-生成器/同步生成器.dart
+++ 30-生成器/同步生成器.dart
@@ -1,3 +1,10 @@
+main(List<String> args) {
+  var it = naturalsTo(5).iterator;
+  while(it.moveNext()) {
+    print(it.current);
+  }
+}
+
 Iterable<int> naturalsTo(int n) sync* {
   print('start');
   int k = 0;
@@ -5,11 +12,4 @@ Iterable<int> naturalsTo(int n) sync* {
     yield k++;
   }
   print('end');
-}
-
-main(List<String> args) {
-  var it = naturalsTo(5).iterator;
-  while(it.moveNext()) {
-    print(it.current);
-  }
 }
